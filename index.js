8cee24da17e83780808233b623daa9a66e00b4fd
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-04 11:07:58 +0530

    phonebook backend step9

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('body', (req) => {
     if (req.method === 'POST')
@@ -8,6 +9,7 @@ morgan.token('body', (req) => {
 })
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
