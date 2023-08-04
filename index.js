cf253d8424a360792e622c4df274572d7d0886fd
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-04 12:07:53 +0530

    added build

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', (req) => {
 })
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
