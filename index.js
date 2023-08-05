56ba0f891024a6dc0b0145d884f5a1031bbeb5bb
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-05 21:14:25 +0530

    Phonebook database step6

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ morgan.token('body', (req) => {
         return JSON.stringify(req.body)
 })
 
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 
 let persons = [
     {
@@ -50,40 +52,38 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
     const date = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+    Person.countDocuments({}).then(count => {
+        response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person)
-        response.json(person)
-    else
-        response.status(404).end()
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const randomId = Math.floor(Math.random() * 1000)
+app.post('/api/persons', (request, response, next) => {
+    //const randomId = Math.floor(Math.random() * 1000)
     const body = request.body
 
     if (body.name === '' || body.number === '') {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+        console.log('error: content missing')
+        return response.status(400).end()
     }
-    //const personList = Person.map(person => person.name)
 
-    //if (personList.includes(body.name)) {
-    //    return response.status(400).json({
-    //        error: 'name must be unique'
-    //    })
-    //}
     //body.id = randomId
     const person = new Person({
         name: body.name,
@@ -92,10 +92,39 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
-    })
+    }).catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedPerson => {
+        response.json(updatedPerson)
+    }).catch(error => next(error))
+})
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
